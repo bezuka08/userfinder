498122efe94115a74ef1ee4def11da72d3799022
Rajya Vardhan <66107299+vardhanrajya08@users.noreply.github.com>
2022-10-24 15:16:15 +0530

    Update userfinder.sh

--- userfinder.sh
+++ userfinder.sh
@@ -8,3 +8,19 @@ B='\033[0;34m'
 Y='\033[0;38m'       	# Yellow
 C='\033[0;36m'         	# Cyan
 W='\033[0;37m'        	# White
+
+echo -e "
+          ${Y}    UserFinder
+     ${C}-> A Tool To Find Username 
+     ${C}-> Across Internet 
+     ${Y}-> OSINT Automaton Tool
+    ${C} -> Scripted By : ${G}Rajya Vardhan
+	${W} -> Twitter   : ${B}https://www.twitter.com/vardhanrajya08
+	${W} -> Instagram : ${B}https://www.twitter.com/vardhanrajya08    
+    ${B} -> Use only for Education purpose
+   
+      
+      
+      ${Y} Usage: bash $0 list.txt Username 
+      ${O} Nothing Is Perfect, Try Again And Make It 
+          "
